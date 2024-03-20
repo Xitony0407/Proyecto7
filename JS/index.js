f7dbf1e24b4f42294ccc87772b53d171196fe13e
Xitony0407 <gutierrezperezximena4@gmail.com>
2024-03-20 15:31:47 -0600

    Proyecto7

---
+++ JS/index.js
@@ -0,0 +1,46 @@
+let resultado = document.getElementById('resultado');
+
+let asignar = (valor) => {
+    resultado.value += valor;
+};
+
+let calcular = () => {
+    resultado.value = eval(resultado.value);
+};
+
+let limpiar = () => {
+    resultado.value = "";
+};
+
+let asignarPi = () => {
+    var resultado = document.getElementById('resultado');
+
+    resultado.value = Math.PI;
+};
+
+let calcularRaizCuadrada = () => {
+    var resultado = document.getElementById('resultado');
+
+    var valorResultado = parseFloat(resultado.value);
+    var raizCuadrada = Math.sqrt(valorResultado);
+
+    resultado.value = raizCuadrada;
+};
+
+let elevarAlCubo = () => {
+    var resultado = document.getElementById('resultado');
+
+    var valorResultado = parseFloat(resultado.value);
+    var resultadoCubo = Math.pow(valorResultado, 3);
+
+    resultado.value = resultadoCubo;
+};
+
+let elevarAlCuadrado = () => {
+    var resultado = document.getElementById('resultado');
+
+    var valorResultado = parseFloat(resultado.value);
+    var resultadoCubo = Math.pow(valorResultado, 2);
+
+    resultado.value = resultadoCubo;
+};
